3432e6d7c44734f604a37744e24d1138c5d80ede
Siddharth Dev Tiwari <sidtiwari98@gmail.com>
2020-12-16 00:07:38 +0800

    Condtion done

--- kodable.hs
+++ kodable.hs
@@ -21,8 +21,18 @@ getDirections x directions= do
         then 
             return directions
         else
-            do
-                getDirections (x+1) (directions ++ [direction]) 
+                getDirections (x+1) (directions ++ parseDirections direction) 
+
+
+parseDirections :: String -> [String]
+parseDirections direction
+        | length direction > 4 && take 4 direction == "Cond" = [ [(direction !! 5)] , take (length(direction) - 1 - 8) (drop 8 direction) ] 
+        | otherwise = [direction]
+
+
+-- getNextDirection :: [String] -> (String, String)
+-- getNextDirection (x:y:ys) = (x,y)
+-- getNextDirection (x:xs) = (x,x)
 
 
 -- implement error handling for file doesnt exist
@@ -40,6 +50,7 @@ load = do
         if (nextCommand == "play") 
                 then do 
                         directions <- getDirections (-1) []
+                        -- showMap directions
                         play map directions '-'
         else
                 do
@@ -49,36 +60,53 @@ load = do
 
 play :: [String] -> [String] -> Char -> IO()
 play _ [] _ = return ()
-play map (d:ds) charOnWhichBallIsSitting = 
+play map directions charOnWhichBallIsSitting = 
         do
-                let updatedMap = makeMove map d charOnWhichBallIsSitting
+                let updatedMap = makeMove map directions charOnWhichBallIsSitting
+                -- showMap directions
                 if (map == updatedMap)
                         then do
-                                putStr "Sorry, error: cannot move to the "
-                                putStrLn d
+                                if (length (head(directions)) == 1)
+                                        then do
+                                                putStr "Sorry, error: The colour "
+                                                putStr (head(directions))
+                                                putStrLn " was not found"
+                                else 
+                                        do        
+                                                putStr "Sorry, error: cannot move to the "
+                                                putStrLn (head(directions))
+                                
                                 putStrLn "Your current Board :"
                                 showMap updatedMap
-                else do
-                        showMap updatedMap   
-                        let bonusEarned = 3 - length (currentPositionOfCharacters updatedMap 'b')
-                        let prevBonus = 3 - length (currentPositionOfCharacters map 'b')
-                        if (bonusEarned /= prevBonus)
-                                then do
-                                        putStr "Got "
-                                        putStr $ show (bonusEarned)
-                                        putStrLn " bonus"
-                                        putStrLn " "  
-                        else
-                                putStrLn " "  
-                        play updatedMap ds '-'
+                        else do
+                                showMap updatedMap   
+                                let bonusEarned = 3 - length (currentPositionOfCharacters updatedMap 'b')
+                                let prevBonus = 3 - length (currentPositionOfCharacters map 'b')
+                                if (bonusEarned /= prevBonus)
+                                        then do
+                                                putStr "Got "
+                                                putStr $ show (bonusEarned)
+                                                putStrLn " bonus"
+                                                putStrLn " "  
+                                else
+                                        putStrLn " " 
+
+                                let (ballXNew, ballYNew) =  head (currentPositionOfCharacters updatedMap '@')
+                                if ([[(map !! ballXNew)!! ballYNew]] == take 1 (tail directions))
+                                        then 
+                                                play updatedMap (drop 2 directions) ((map !! ballXNew)!! ballYNew)
+                                else
+                                        play updatedMap (tail directions) ((map !! ballXNew)!! ballYNew)   
 
 
-makeMove :: [String] -> String -> Char -> [String]
-makeMove map direction charOnWhichBallIsSitting
-        | direction == "r" = take ballRow map ++ [moveBallRight (map !! ballRow) charOnWhichBallIsSitting ballColumn] ++  drop(ballRow + 1) map
-        | direction == "l" = take ballRow map ++ [(moveBallLeft (map !! ballRow) charOnWhichBallIsSitting ballColumn)] ++  drop(ballRow + 1) map
-        | direction == "u" = moveBallUp map charOnWhichBallIsSitting (ballRow, ballColumn)
-        | direction == "d" = moveBallDown map charOnWhichBallIsSitting (ballRow, ballColumn)
+
+makeMove :: [String] -> [String] -> Char -> [String]
+makeMove map (d:ds) charOnWhichBallIsSitting
+        | d == "Right" = take ballRow map ++ [moveBallRight (map !! ballRow) charOnWhichBallIsSitting ballColumn (take 1 ds)] ++  drop(ballRow + 1) map
+        | d == "Left" = take ballRow map ++ [moveBallLeft (map !! ballRow) charOnWhichBallIsSitting ballColumn (take 1 ds)] ++  drop(ballRow + 1) map
+        | d == "Up" = moveBallUp map charOnWhichBallIsSitting (ballRow, ballColumn) (take 1 ds)
+        | d == "Down" = moveBallDown map charOnWhichBallIsSitting (ballRow, ballColumn) (take 1 ds)
+        | otherwise = map
         where
                 positionOfBall = currentPositionOfCharacters map '@'
                 ballRow = fst(head(positionOfBall))
@@ -86,13 +114,25 @@ makeMove map direction charOnWhichBallIsSitting
 
 
 
-moveBallUp ::  [String] -> Char -> (Int, Int) -> [String]
-moveBallUp map charOnWhichBallIsSitting (ballRow, ballColumn) = 
+
+moveBallUp ::  [String] -> Char -> (Int, Int) -> [String] -> [String]
+moveBallUp map charOnWhichBallIsSitting (ballRow, ballColumn) nextDir= 
         if (ballRow > 0)
                 then 
                         case nextChar of
-                                '-' -> moveBallUp (moveBallUpOnce map charOnWhichBallIsSitting (ballRow, ballColumn)) nextChar (ballRow -1, ballColumn)
-                                'b' -> moveBallUp (moveBallUpOnce map charOnWhichBallIsSitting (ballRow, ballColumn)) '-' (ballRow -1, ballColumn)
+                                '-' -> moveBallUp (moveBallUpOnce map charOnWhichBallIsSitting (ballRow, ballColumn)) nextChar (ballRow -1, ballColumn) nextDir
+                                
+                                'b' -> moveBallUp (moveBallUpOnce map charOnWhichBallIsSitting (ballRow, ballColumn)) '-' (ballRow -1, ballColumn) nextDir
+                                
+                                'y' ->  if (nextDir == [['y']]) then moveBallUpOnce map charOnWhichBallIsSitting (ballRow, ballColumn)
+                                        else moveBallUp (moveBallUpOnce map charOnWhichBallIsSitting (ballRow, ballColumn)) nextChar (ballRow -1, ballColumn) nextDir
+                                
+                                'o' ->  if (nextDir == [['o']]) then moveBallUpOnce map charOnWhichBallIsSitting (ballRow, ballColumn)
+                                        else moveBallUp (moveBallUpOnce map charOnWhichBallIsSitting (ballRow, ballColumn)) nextChar (ballRow -1, ballColumn) nextDir
+                                
+                                'p' ->  if (nextDir == [['p']]) then moveBallUpOnce map charOnWhichBallIsSitting (ballRow, ballColumn)
+                                        else moveBallUp (moveBallUpOnce map charOnWhichBallIsSitting (ballRow, ballColumn)) nextChar (ballRow -1, ballColumn) nextDir
+                                
                                 _   -> map
         else map
         where 
@@ -106,13 +146,24 @@ moveBallUpOnce map charOnWhichBallIsSitting (ballRow, ballColumn) =
                 rowUp = [take ballColumn (map !! (ballRow -1)) ++ ['@'] ++ drop (ballColumn + 1) (map !! (ballRow -1))]
                 rowCurrent =[take ballColumn (map !! ballRow ) ++ [charOnWhichBallIsSitting] ++ drop (ballColumn + 1) (map !! ballRow)]
 
-moveBallDown ::  [String] -> Char -> (Int, Int) -> [String]
-moveBallDown map charOnWhichBallIsSitting (ballRow, ballColumn) = 
+moveBallDown ::  [String] -> Char -> (Int, Int) -> [String] -> [String]
+moveBallDown map charOnWhichBallIsSitting (ballRow, ballColumn) nextDir = 
         if (ballRow < length map)
                 then 
                         case nextChar of
-                                '-' -> moveBallDown (moveBallDownOnce map charOnWhichBallIsSitting (ballRow, ballColumn)) nextChar (ballRow +1, ballColumn)
-                                'b' -> moveBallDown (moveBallDownOnce map charOnWhichBallIsSitting (ballRow, ballColumn)) '-' (ballRow +1, ballColumn)
+                                '-' -> moveBallDown (moveBallDownOnce map charOnWhichBallIsSitting (ballRow, ballColumn)) nextChar (ballRow +1, ballColumn) nextDir
+                                
+                                'b' -> moveBallDown (moveBallDownOnce map charOnWhichBallIsSitting (ballRow, ballColumn)) '-' (ballRow +1, ballColumn) nextDir
+                                
+                                'y' ->  if (nextDir == [['y']]) then moveBallDownOnce map charOnWhichBallIsSitting (ballRow, ballColumn)
+                                        else moveBallDown (moveBallDownOnce map charOnWhichBallIsSitting (ballRow, ballColumn)) nextChar (ballRow +1, ballColumn) nextDir
+                                
+                                'o' ->  if (nextDir == [['o']]) then moveBallDownOnce map charOnWhichBallIsSitting (ballRow, ballColumn)
+                                        else moveBallDown (moveBallDownOnce map charOnWhichBallIsSitting (ballRow, ballColumn)) nextChar (ballRow +1, ballColumn) nextDir
+                                
+                                'p' ->  if (nextDir == [['p']]) then moveBallDownOnce map charOnWhichBallIsSitting (ballRow, ballColumn)
+                                        else moveBallDown (moveBallDownOnce map charOnWhichBallIsSitting (ballRow, ballColumn)) nextChar (ballRow +1, ballColumn) nextDir
+                                
                                 _   -> map
         else map
         where 
@@ -126,14 +177,25 @@ moveBallDownOnce map charOnWhichBallIsSitting (ballRow, ballColumn) =
                 rowDown = [take ballColumn (map !! (ballRow +1)) ++ ['@'] ++ drop (ballColumn + 1) (map !! (ballRow +1))]
                 rowCurrent =[take ballColumn (map !! ballRow) ++ [charOnWhichBallIsSitting] ++ drop (ballColumn + 1) (map !! ballRow)]
 
-moveBallRight :: String -> Char -> Int -> String
-moveBallRight s charOnWhichBallIsSitting ballposition = 
+moveBallRight :: String -> Char -> Int -> [String] -> String
+moveBallRight s charOnWhichBallIsSitting ballposition nextDir = 
         if (ballposition + 2 < length s)
         then 
                 case s !! (ballposition + 2) of 
-                        '-' -> moveBallRight (moveBallRightOnce s charOnWhichBallIsSitting ballposition)  (s !! (ballposition + 2)) (ballposition + 2)
-                        'b' -> moveBallRight (moveBallRightOnce s charOnWhichBallIsSitting ballposition)  '-' (ballposition + 2)
-                        _ -> s
+                        '-' -> moveBallRight (moveBallRightOnce s charOnWhichBallIsSitting ballposition)  (s !! (ballposition + 2)) (ballposition + 2) nextDir
+                        
+                        'b' -> moveBallRight (moveBallRightOnce s charOnWhichBallIsSitting ballposition)  '-' (ballposition + 2) nextDir
+                        
+                        'y' ->  if (nextDir == [['y']]) then moveBallRightOnce s charOnWhichBallIsSitting ballposition
+                                else moveBallRight (moveBallRightOnce s charOnWhichBallIsSitting ballposition)  (s !! (ballposition + 2)) (ballposition + 2) nextDir 
+                        
+                        'o' ->  if (nextDir == [['o']]) then moveBallRightOnce s charOnWhichBallIsSitting ballposition
+                                else moveBallRight (moveBallRightOnce s charOnWhichBallIsSitting ballposition)  (s !! (ballposition + 2)) (ballposition + 2) nextDir 
+                        
+                        'p' ->  if (nextDir == [['p']]) then moveBallRightOnce s charOnWhichBallIsSitting ballposition
+                                else moveBallRight (moveBallRightOnce s charOnWhichBallIsSitting ballposition)  (s !! (ballposition + 2)) (ballposition + 2) nextDir 
+                        
+                        _ -> s            
         else s
 
 moveBallRightOnce :: String -> Char -> Int -> String
@@ -141,13 +203,24 @@ moveBallRightOnce s charOnWhichBallIsSitting ballposition =
         take ballposition s ++ [charOnWhichBallIsSitting] ++ [' '] ++ ['@'] ++ [' '] ++ drop (ballposition + 4) s
 
 
-moveBallLeft :: String -> Char -> Int -> String
-moveBallLeft s charOnWhichBallIsSitting ballposition = 
+moveBallLeft :: String -> Char -> Int -> [String] -> String
+moveBallLeft s charOnWhichBallIsSitting ballposition nextDir = 
         if (ballposition - 2 >= 0)
         then 
                 case s !! (ballposition - 2) of 
-                        '-' -> moveBallLeft (moveBallLeftOnce s charOnWhichBallIsSitting ballposition)  (s !! (ballposition - 2)) (ballposition - 2)
-                        'b' -> moveBallLeft (moveBallLeftOnce s charOnWhichBallIsSitting ballposition)  '-' (ballposition - 2)
+                        '-' -> moveBallLeft (moveBallLeftOnce s charOnWhichBallIsSitting ballposition)  (s !! (ballposition - 2)) (ballposition - 2) nextDir
+                        
+                        'b' -> moveBallLeft (moveBallLeftOnce s charOnWhichBallIsSitting ballposition)  '-' (ballposition - 2) nextDir
+                        
+                        'y' ->  if (nextDir == [['y']]) then moveBallLeftOnce s charOnWhichBallIsSitting ballposition
+                                else moveBallLeft (moveBallLeftOnce s charOnWhichBallIsSitting ballposition)  (s !! (ballposition - 2)) (ballposition - 2) nextDir
+                        
+                        'o' ->  if (nextDir == [['o']]) then moveBallLeftOnce s charOnWhichBallIsSitting ballposition
+                                else moveBallLeft (moveBallLeftOnce s charOnWhichBallIsSitting ballposition)  (s !! (ballposition - 2)) (ballposition - 2) nextDir 
+                        
+                        'p' ->  if (nextDir == [['p']]) then moveBallLeftOnce s charOnWhichBallIsSitting ballposition
+                                else moveBallLeft (moveBallLeftOnce s charOnWhichBallIsSitting ballposition)  (s !! (ballposition - 2)) (ballposition - 2) nextDir
+                        
                         _ -> s
         else s
 
@@ -162,6 +235,4 @@ showMap :: [String] -> IO()
 showMap [] = return ()
 showMap (l:ls) = do
     putStrLn l
-    showMap ls
-
-            
+    showMap ls         
